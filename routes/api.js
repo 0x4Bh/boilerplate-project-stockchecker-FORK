fefcdbaf0cf4e35ecd3137a14102f33ae6d7e0fa
0x4Bh <kadir3407@gmail.com>
2023-09-17 17:31:49 +0300

    finish: all tasks got completed

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,90 @@
-'use strict';
+"use strict";
+
+const axios = require("axios");
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const mySecret = process.env.DB;
+mongoose.connect(mySecret, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "stockchecker",
+});
+
+const ipLikeSchema = new mongoose.Schema({
+  ip: { type: String },
+  symbol: { type: String },
+});
+ipLikeSchema.index({ ip: 1, symbol: 1 }, { unique: true });
+const IPLikeModel = mongoose.model("ip", ipLikeSchema);
 
 module.exports = function (app) {
+  app.route("/api/stock-prices").get(async function (req, res) {
+    let reqIP = req.ip;
+    const { stock, like } = req.query;
+    let [stock1, stock2] = Array.isArray(stock)
+      ? [...stock]
+      : [stock, undefined];
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+    // ////////
+    // reqIP = "155.155.155.0"
+    ///////
+
+    if (like == "true") {
+      let newIpLike1 = new IPLikeModel({ ip: reqIP, symbol: stock1 });
+      try {
+        await newIpLike1.save();
+      } catch (error) {
+        console.log("stock1 duplicate");
+      }
+
+      let newIpLike2 = {};
+      if (stock2) {
+        newIpLike2 = new IPLikeModel({ ip: reqIP, symbol: stock2 });
+        try {
+          await newIpLike2.save();
+        } catch (error) {
+          console.log("stock2 duplicate");
+        }
+      }
+    }
+
+    const urls = [];
+    urls[0] = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock1}/quote`;
+    if (stock2) {
+      urls[1] = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock2}/quote`;
+    }
+
+    const prices = [];
+    for (let urlItem of urls) {
+      await axios({
+        method: "get",
+        url: urlItem,
+      }).then((apiResp) => {
+        prices.push(apiResp.data.latestPrice);
+      });
+    }
+
+    const likes1 = await IPLikeModel.countDocuments({ symbol: stock1 }).exec();
+    if (stock2) {
+      const likes2 = await IPLikeModel.countDocuments({
+        symbol: stock2,
+      }).exec();
+      const stockObj1 = {
+        stock: stock1,
+        price: prices[0],
+        rel_likes: likes1 - likes2,
+      };
+      const stockObj2 = {
+        stock: stock2,
+        price: prices[1],
+        rel_likes: likes2 - likes1,
+      };
+      return res.json({ stockData: [stockObj1, stockObj2] });
+    } else {
+      return res.json({
+        stockData: { stock: stock1, price: prices[0], likes: likes1 },
+      });
+    }
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,121 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  test("1. Viewing one stock: GET request to /api/stock-prices/", (done) => {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .set("content-type", "application/json")
+      .query({ stock: "A" })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.property(res.body.stockData, "stock");
+        assert.equal(res.body.stockData.stock, "A");
+        assert.property(res.body.stockData, "price");
+        assert.exists(res.body.stockData.price);
+        assert.property(res.body.stockData, "likes");
+        done();
+      });
+  });
 
+  let likeAmount = 0;
+  test("2. Viewing one stock and liking it: GET request to /api/stock-prices/", (done) => {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .set("content-type", "application/json")
+      .query({ stock: "A", like: "true" })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.property(res.body.stockData, "stock");
+        assert.equal(res.body.stockData.stock, "A");
+        assert.property(res.body.stockData, "price");
+        assert.exists(res.body.stockData.price);
+        assert.property(res.body.stockData, "likes");
+        assert.isAbove(res.body.stockData.likes, 0);
+        likeAmount = res.body.stockData.likes;
+        done();
+      });
+  });
+  test("3. Viewing the same stock and liking it again: GET request to /api/stock-prices/", (done) => {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .set("content-type", "application/json")
+      .query({ stock: "A", like: "true" })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.property(res.body.stockData, "stock");
+        assert.equal(res.body.stockData.stock, "A");
+        assert.property(res.body.stockData, "price");
+        assert.exists(res.body.stockData.price);
+        assert.property(res.body.stockData, "likes");
+        assert.equal(res.body.stockData.likes, likeAmount);
+        done();
+      });
+  });
+
+  let relLikesMSFT = 0;
+  let relLikesAAPL = 0;
+  let difference = 0;
+  test("4. Viewing two stocks: GET request to /api/stock-prices/", (done) => {
+    chai
+      .request(server)
+      .get("/api/stock-prices?stock=msft&stock=aapl")
+      .set("content-type", "application/json")
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.isArray(res.body.stockData);
+        res.body.stockData.forEach((stockItem) => {
+          assert.property(stockItem, "stock");
+          assert.oneOf(stockItem.stock, ["msft", "aapl"]);
+          assert.property(stockItem, "price");
+          assert.exists(stockItem.price);
+          assert.property(stockItem, "rel_likes");
+          assert.exists(stockItem.rel_likes);
+        });
+        relLikesMSFT = res.body.stockData[0].rel_likes;
+        relLikesAAPL = res.body.stockData[1].rel_likes;
+        difference = relLikesMSFT - relLikesAAPL;
+        done();
+      });
+  });
+  test("5. Viewing two stocks and liking them: GET request to /api/stock-prices/", (done) => {
+    chai
+      .request(server)
+      .get("/api/stock-prices?stock=msft&stock=aapl&like=true")
+      .set("content-type", "application/json")
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.isArray(res.body.stockData);
+        res.body.stockData.forEach((stockItem) => {
+          assert.property(stockItem, "stock");
+          assert.oneOf(stockItem.stock, ["msft", "aapl"]);
+          assert.property(stockItem, "price");
+          assert.exists(stockItem.price);
+          assert.property(stockItem, "rel_likes");
+          assert.exists(stockItem.rel_likes);
+        });
+        assert.equal(
+          res.body.stockData[0].rel_likes - res.body.stockData[1].rel_likes,
+          difference
+        );
+        done();
+      });
+  });
 });
